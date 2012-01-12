2d1a30c40317bf88afc0b4bfaa1a09903176c69d
Grady Simon <grady@gradysimon.com>
2012-01-12 16:50:39 -0500

    Coded the referral command methods. They are currently missing error messages (as are all of the commands) and output generation, but their logic should be done.

--- src/com/gradysimon/reputationweb/ReputationCommandExecutor.java
+++ src/com/gradysimon/reputationweb/ReputationCommandExecutor.java
@@ -1,10 +1,15 @@
 package com.gradysimon.reputationweb;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.bukkit.OfflinePlayer;
 import org.bukkit.Server;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.command.RemoteConsoleCommandSender;
 import org.bukkit.entity.Player;
 
 public class ReputationCommandExecutor implements CommandExecutor {
@@ -43,6 +48,7 @@ public class ReputationCommandExecutor implements CommandExecutor {
 			case "referral":
 			case "reference":
 			case "ref":
+			case "connection":
 				return referralCommand(sender, args);
 
 			default:
@@ -54,8 +60,67 @@ public class ReputationCommandExecutor implements CommandExecutor {
 	}
 
 	private boolean referralCommand(CommandSender sender, String[] args) {
-		// TODO Auto-generated method stub
+		if (args.length == 2) {
+			return selfReferralCommand(sender, args);
+		}
+		if (args.length == 3) {
+			return otherReferralCommand(sender, args);
+		}
 		return false;
+	}
+
+	private boolean otherReferralCommand(CommandSender sender, String[] args) {
+		Player startPlayer = getRealPlayer(args[1]);
+		Player endPlayer = getRealPlayer(args[2]);
+		if (startPlayer == null || endPlayer == null) {
+			// TODO: At least one of the players doesn't exist error
+			return true;
+		}
+		List<Player> path = reputationGraph
+				.getReference(startPlayer, endPlayer);
+		if (path == null) {
+			// If this is null, there is no path between them
+			// TODO: ensure that getReference returns null if there is no
+			// path.
+			// TODO: no path exists error.
+			return true;
+		}
+		List<String> namesInPath = convertToNameList(path);
+		String output = generateReferralOutput(namesInPath,
+				startPlayer.getName(), endPlayer.getName());
+		sender.sendMessage(output);
+		return true;
+	}
+
+	private boolean selfReferralCommand(CommandSender sender, String[] args) {
+		if (!isPlayer(sender)) {
+			// TODO: must be a player or must supply 2 names error
+		}
+		Player senderPlayer = (Player) sender;
+		Player otherPlayer = getRealPlayer(args[1]);
+		if (otherPlayer == null) {
+			// TODO: other player doesn't exist error
+			return true;
+		}
+		List<Player> path = reputationGraph.getReference(senderPlayer,
+				otherPlayer);
+		if (path == null) {
+			// If this is null, there is no path between them
+			// TODO: ensure that getReference returns null if there is no
+			// path.
+			// TODO: no path exists error.
+		}
+		List<String> namesInPath = convertToNameList(path);
+		String output = generateReferralOutput(namesInPath,
+				senderPlayer.getName(), otherPlayer.getName());
+		return false;
+	}
+
+	private String generateReferralOutput(List<String> path, String startName,
+			String endName)
+	{
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 	private boolean infoCommand(CommandSender sender, String[] args) {
@@ -76,7 +141,7 @@ public class ReputationCommandExecutor implements CommandExecutor {
 							// the server
 		}
 		if (!reputationGraph.trustRelationExists(commandSender, otherPlayer)) {
-			// TODO: don't already trusts error message
+			// TODO: don't already trust error message
 			return true;
 		}
 		reputationGraph.removeTrustRelation(commandSender, otherPlayer);
@@ -109,11 +174,24 @@ public class ReputationCommandExecutor implements CommandExecutor {
 		return sender instanceof Player;
 	}
 
+	private boolean isConsole(CommandSender sender) {
+		return (sender instanceof ConsoleCommandSender)
+				|| (sender instanceof RemoteConsoleCommandSender);
+	}
+
 	private Player getRealPlayer(String name) {
 		OfflinePlayer potentialPlayer = server.getOfflinePlayer(name);
 		if (potentialPlayer.hasPlayedBefore()) {
 			return potentialPlayer.getPlayer();
 		}
 		return null;
+	}
+
+	private List<String> convertToNameList(List<Player> playerList) {
+		List<String> nameList = new ArrayList<String>(playerList.size());
+		for (Player player : playerList) {
+			nameList.add(player.getName());
+		}
+		return nameList;
 	}
 }
